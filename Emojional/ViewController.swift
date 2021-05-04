d9a4fab2d9e00d721ee82da577a0e21e045e3556
Megan and Grace <meganrmulrooney@gmail.com>
2021-05-04 07:32:15 -0500

    Initial Commit

---
+++ Emojional/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Emojional
+//
+//  Created by Mark Mulrooney on 5/4/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
