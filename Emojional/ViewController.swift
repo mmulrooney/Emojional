89b62701f1cdb08cb84a5147acd5aa2a9bc47987
Megan and Grace <meganrmulrooney@gmail.com>
2021-05-19 21:59:04 -0500

    Emojional Project for KWK IA Training

--- Emojional/ViewController.swift
+++ Emojional/ViewController.swift
@@ -8,12 +8,38 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    let emojis = [
+        "🤠" : "cowboy",
+        "😚" : "smiley"
+    ]
+    
+    let customMessages = [
+        "cowboy" : ["you're a cowboy like me",
+                    "he never wanted love, just a fancy car",
+                    "dancing is a dangerous game" ],
+        "smiley" : ["all's well that ends well",
+                    "darling, I fancy you",
+                    "feeling so Gatsby for that whole year"
+        ]
+    ]
+    
+    
+       @IBAction func showMessage(sender: UIButton) {
+           let selectedEmotion = (sender.titleLabel?.text)!
+           let options = customMessages[emojis[selectedEmotion]!]!
+           let emojiMessage = options.randomElement()
+           let alertController = UIAlertController(title: "Alert!", message: emojiMessage, preferredStyle: UIAlertController.Style.alert)
+           alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+           present(alertController, animated: true, completion: nil)
+       }
 
+    
 }
 
